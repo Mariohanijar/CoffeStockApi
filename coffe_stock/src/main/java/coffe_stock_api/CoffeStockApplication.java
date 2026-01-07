02a31394fea031b523fca27bf43d20c4acc15cf7
Mariohanijar <124818688+Mariohanijar@users.noreply.github.com>
2026-01-07 13:27:08 -0300

    começando o projeto

---
+++ coffe_stock/src/main/java/coffe_stock_api/CoffeStockApplication.java
@@ -0,0 +1,13 @@
+package coffe_stock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeStockApplication.class, args);
+	}
+
+}
---
+++ coffe_stock/src/test/java/coffe_stock_api/CoffeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package coffe_stock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
